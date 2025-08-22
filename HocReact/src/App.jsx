a93a44dc69c76dfa2d2c4ec9f6ea9efaf67f7099
Lê Đăng Khoa <khoald.24it@vku.udn.vn>
2025-08-22 21:25:44 +0700

    Update code lần 5

--- HocReact/src/App.jsx
+++ HocReact/src/App.jsx
@@ -1,47 +1,17 @@
-import { useState } from 'react'
-import MyComponents from "./components/learn/MyComponents"
-import SecondComponents from "./components/learn/SecondComponents"
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import './components/todo/todo.css'
+import TodoNew from "./components/todo/TodoNew"
+import TodoData from './components/todo/TodoData'
+import reactLogo from './assets/react.svg';
 const App = () => {
-  const [count, setCount] = useState(0)
-
-  // arrow Funtion
-  // function myFuntion(){
-  //   console.log("run my Funtion");
-  // }
-
-  const myFuntion = () => {
-    console.log("run my Funtion");
-  }
-
-  myFuntion();
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="todo=container">
+      <div className="todo-title">Todo list</div>
+      <TodoNew/>
+      <TodoData/>
+      <div className='todo-image'>
+        <img src={reactLogo} alt="Anh" />
       </div>
-      <h1>Hello World</h1>
-      <MyComponents/>
-      <SecondComponents/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
---
+++ HocReact/src/components/todo/TodoNew.jsx
@@ -0,0 +1,10 @@
+const TodoNew = () => {
+    return (
+    <div>
+        <input type="text" placeholder="hãy nhập điều mà bạn muốn"/>
+        <button>Add</button>
+    </div>
+    )
+}
+
+export default TodoNew;
