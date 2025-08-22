631ff7e2090a932721c9f2ff64877f34425bb810
Lê Đăng Khoa <khoald.24it@vku.udn.vn>
2025-08-22 20:03:40 +0700

    Update code mới lần 4

--- HocReact/src/components/learn/MyComponents.jsx
+++ HocReact/src/components/learn/MyComponents.jsx
@@ -1,5 +1,21 @@
 export default function MyComponents() {
+    // const aykuBin = "Bin ne 1"; // string
+    // const aykuBin = 11; // number
+    // const aykuBin = true; // boolean
+    // const aykuBin = undefined; 
+    // const aykuBin = null;
+    // const aykuBin = [1, 2, 3];
+    const aykuBin = {
+        name: "Bin",
+        age: 19
+    }
     return (
-        <div>Bin ne</div>
+        <>
+            <div>Bin ne</div>
+            <div>{JSON.stringify(aykuBin)}</div>
+            <div>Ten la: {aykuBin.name}</div>
+            <div>Tuoi la: {aykuBin.age}</div>
+            <div>{console.log("Eric")}</div>
+        </>
     );
 }
