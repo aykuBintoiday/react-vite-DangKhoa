f0cb52d8750f3a82f419b5a2cfba4c9549deb295
Lê Đăng Khoa <khoald.24it@vku.udn.vn>
2025-08-24 15:14:50 +0700

    Update code lần 6

--- HocReact/src/App.jsx
+++ HocReact/src/App.jsx
@@ -4,11 +4,23 @@ import TodoData from './components/todo/TodoData'
 import reactLogo from './assets/react.svg';
 const App = () => {
 
+  const Bin = "Bin haha";
+  const age = 19;
+  const data = {
+    address: "danang",
+    country: "vietnam"
+  }
+
   return (
     <div className="todo=container">
       <div className="todo-title">Todo list</div>
       <TodoNew/>
-      <TodoData/>
+      <TodoData
+      name={Bin}
+      age={age}
+      address={data.address}
+      country={data.country}
+      />
       <div className='todo-image'>
         <img src={reactLogo} alt="Anh" />
       </div>
--- HocReact/src/components/todo/TodoData.jsx
+++ HocReact/src/components/todo/TodoData.jsx
@@ -1,6 +1,19 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    //props là một object {}
+    // {
+    //     name: "Bin",
+    //     age: 19,
+    //     data: {}
+    // }
+    // const {name, age, data } = props;
+    // const name = props.name;
+    // const age = props.age;
+    // const data = props.data;
+
+    // console.log(">>> check props: ", props);
     return (
     <div className='todo-data'>
+        <div>My name is : {props.name} </div>
         <div>Learning React</div>
         <div>Watching Youtube</div>
     </div>
