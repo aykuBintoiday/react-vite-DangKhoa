0a1a661508e4185c47c8ffea019ab8dd69ac0eab
Lê Đăng Khoa <khoald.24it@vku.udn.vn>
2025-08-24 16:08:22 +0700

    Update code lần 8

--- HocReact/src/components/todo/TodoNew.jsx
+++ HocReact/src/components/todo/TodoNew.jsx
@@ -3,10 +3,17 @@ const TodoNew = (props) => {
     const {addNewTodo} = props;
 
     // addNewTodo("Bin");
+    const handleClick = () => {
+        alert("Click me");
+    }
+
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange", name);
+    }
     return (
     <div>
-        <input type="text" placeholder="hãy nhập điều mà bạn muốn"/>
-        <button>Add</button>
+        <input type="text" placeholder="hãy nhập điều mà bạn muốn" onChange={(event) => {handleOnChange(event.target.value)}}/>
+        <button onClick={handleClick}>Add</button>
     </div>
     )
 }
