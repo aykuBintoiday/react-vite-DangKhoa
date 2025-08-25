08c61ce6a2074b588ef0a93f60c215f539d9a989
Lê Đăng Khoa <khoald.24it@vku.udn.vn>
2025-08-25 21:59:56 +0700

    Update code lần 16

--- HocReact/src/main.jsx
+++ HocReact/src/main.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/home",
+    element: <div>Home page</div>,
+  },
+  {
+    path: "/users",
+    element: <div>Users page</div>,
+  },
+  {
+    path: "/products",
+    element: <div>Products page</div>,
+  },
+  {
+    path: "/login",
+    element: <div>Login page</div>,
+  },
+  {
+    path: "/register",
+    element: <div>Register page</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
